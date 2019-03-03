85de590e0923f4196cf0d731b7346b932c81ffcf
XuJiayao <xujiayao91@163.com>
2019-03-03 13:40:51 +0800

    app入口

---
+++ SFAppEntry/SFAppEntry.h
@@ -0,0 +1,13 @@
+//
+//  SFAppEntry.h
+//  SFAppEntry
+//
+//  Created by XJY on 2019/3/3.
+//  Copyright © 2019 com.sf.ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFAppEntry : NSObject
+
+@end
---
+++ SFAppEntry/SFAppEntry.m
@@ -0,0 +1,13 @@
+//
+//  SFAppEntry.m
+//  SFAppEntry
+//
+//  Created by XJY on 2019/3/3.
+//  Copyright © 2019 com.sf.ios. All rights reserved.
+//
+
+#import "SFAppEntry.h"
+
+@implementation SFAppEntry
+
+@end
